b694ce8bf86c7b0c0dc2732a411a6fda1df09d20
vika728 <vika.sokolova728@gmail.com>
2021-06-28 18:09:51 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+
+let name = prompt('enter u name');
+let numberOfFilms = prompt('How many films did u watch?');
+let moviePersonal = prompt('One of the Latest movie thats u had watch?');
+let chekOut = prompt('How u chek that?');
+
+
+let personalMovieDB = {
+    count : numberOfFilms,
+    movies : {
+        [moviePersonal] : chekOut
+    },
+    actors : {},
+    genres: [],
+    privat: false
+}
+
+for (i = 0; i < 1; i++) {
+    if(name != '' && moviePersonal != '' && chekOut != '' && name != null && moviePersonal != null && chekOut != null && moviePersonal.length > 50) {
+        personalMovieDB.movies[moviePersonal] = chekOut;
+        alert(`${name}, thanks `);
+    }else{
+        console.log('error');;
+        i--;
+    }
+}
+
+if (personalMovieDB.count<10){
+    alert(`${name}, Too less`)
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30){
+    alert(`${name}, U a normal`)
+} else if (personalMovieDB.count > 30) {
+    alert(`${name}, U a cinemalover`)
+} else {
+    alert(`${name}, error`)
+}
+console.log(personalMovieDB);
+// console.log(personalMovieDB.movies);
